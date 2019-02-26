c272dea0a9b59ebbb9e2849fd59282d35a9d68d2
Matteo Muscas <thatman.j@gmail.com>
2019-02-26 10:24:02 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,19 +2,22 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+
   def self.table_name
-    "#{self.to_s.downcase}s"
+    self.to_s.downcase.pluralize
   end
 
   def self.column_names
 
-    sql = "pragma table_info('#{table_name}')"
+    sql = "PRAGMA table_info('#{table_name}')"
 
     table_info = DB[:conn].execute(sql)
     column_names = []
-    table_info.each do |row|
-      column_names << row["name"]
+
+    table_info.each do |column|
+      column_names << column["name"]
     end
+
     column_names.compact
   end
 
@@ -24,14 +27,12 @@ class InteractiveRecord
     end
   end
 
-  def save
-    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
-    DB[:conn].execute(sql)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  def table_name_for_insert
+   self.class.table_name
   end
 
-  def table_name_for_insert
-    self.class.table_name
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
 
   def values_for_insert
@@ -42,8 +43,10 @@ class InteractiveRecord
     values.join(", ")
   end
 
-  def col_names_for_insert
-    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
   def self.find_by_name(name)
@@ -51,10 +54,11 @@ class InteractiveRecord
     DB[:conn].execute(sql)
   end
 
-  def self.find_by(attribute_hash)
-    value = attribute_hash.values.first
-    formatted_value = value.class == Fixnum ? value : "'#{value}'"
-    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = #{formatted_value}"
+  def self.find_by(hash)
+    key = hash.keys[0]
+    sql = "SELECT * FROM #{self.table_name} WHERE #{key.to_s} = '#{hash[key]}'"
     DB[:conn].execute(sql)
   end
+
+
 end
